7ca1708919335d05918affc6a33c77fbf060a2c7
mohammedyasirsuara <55495311+mohammedyasirsuara@users.noreply.github.com>
2019-09-19 19:08:44 -0400

    Update udpSender.py

--- udpSender.py
+++ udpSender.py
@@ -22,7 +22,6 @@ while 1:
         print ("data sent: " + data1)
 #        s.sendall(data.encode('utf-8'))
         s.sendto(data1.encode('utf-8'), server_address)    
-    if not len(data):
-        break
+    
 s.shutdown(1)
 
