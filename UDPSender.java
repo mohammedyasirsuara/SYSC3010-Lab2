0fe8ca4c157278f61e7634d4bfb19177c93ec2b2
mohammedyasirsuara <55495311+mohammedyasirsuara@users.noreply.github.com>
2019-09-20 22:54:35 -0400

    Update UDPSender.java

--- UDPSender.java
+++ UDPSender.java
@@ -2,36 +2,57 @@ import java.net.*;
 import java.util.Scanner;
 
 public class UDPSender {
+	
+	private final static int PACKETSIZE = 100 ;
 
 	public static void main(String[] args) 
    {
 	      // Check the arguments
-	      if( args.length != 2 )
+	      if( args.length != 3 )
 	      {
-	         System.out.println( "usage: java UDPSender host port" ) ;
+	         System.out.println( "usage: java UDPSender host port n messages" ) ;
 	         return ;
 	      }
 	      DatagramSocket socket = null ;
+	      DatagramSocket ackSocket = null;
 	      try
 	      {
 	         // Convert the arguments first, to ensure that they are valid
 	         InetAddress host = InetAddress.getByName( args[0] ) ;
 	         int port         = Integer.parseInt( args[1] ) ;
+	         int nMsgs		  = Integer.parseInt(args[2]);
 	         socket = new DatagramSocket() ;
+	         
+	         // Prepare ACK receiving
+	         ackSocket = new DatagramSocket( port + 1 ) ;
      
-	         Scanner in;
-	         in = new Scanner (System.in);
 	         String message = null;
-	         while (true)
-	         {
-	        		 System.out.println("Enter text to be sent, ENTER to quit ");
-	        		 message = in.nextLine();
-	        		 if (message.length()==0) break;
-	        		 byte [] data = message.getBytes() ;
+
+    		 System.out.println("Sending messages:");
+	         boolean exit = true;
+    		 while (exit) {
+	    		 for (int i = 0; i < nMsgs ; i++) {
+	    			 message = "Message " + Integer.toString(i  + 1);
+	    			 byte [] data = message.getBytes() ;
 	        		 DatagramPacket packet = new DatagramPacket( data, data.length, host, port ) ;
 	        		 socket.send( packet ) ;
-	         } 
-	         System.out.println ("Closing down");
+	        		 System.out.println(new String(packet.getData()).trim());
+	        		 
+	        		 DatagramPacket ackPacket = new DatagramPacket( new byte[PACKETSIZE], PACKETSIZE ) ;
+	 	             ackSocket.receive( ackPacket ) ;
+	 	             System.out.println( ackPacket.getAddress() + " " + ackPacket.getPort() + ": " + new String(ackPacket.getData()).trim() ) ;
+
+	 	             if ( !new String(ackPacket.getData()).trim().equals("ACK: " + message ) ) {
+	 	            	System.out.println("Packet dropped, closing connection");
+	 		            socket.close() ;
+	 		            ackSocket.close();
+	 	            	exit = false;
+	 	             }
+	        		 
+	    		 }
+	    		 exit = false;
+    		 }
+    		 
 	      }
 	      catch( Exception e )
 	      {
@@ -40,7 +61,9 @@ public class UDPSender {
 	      finally
 	      {
 	         if( socket != null )
+	        	System.out.println("Script finished, closing connection");
 	            socket.close() ;
+	            ackSocket.close();
       }
    }
 }
